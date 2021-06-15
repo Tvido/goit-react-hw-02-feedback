92bb09cff4faf37376b5ef044583d57f9dd1d030
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-16 00:40:13 +0300

    Update Message.js

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Notification = ({ message }) => (
   <>
     <h2 className="">{message}</h2>
   </>
 );
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default Notification;
